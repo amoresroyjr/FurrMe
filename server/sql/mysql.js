cc73da241466788cde809dab8761350e31cb8119
amoresroyjr <amoresroyjr@gmail.com>
2025-05-29 08:15:53 +0800

    db aiven update

--- server/sql/mysql.js
+++ server/sql/mysql.js
@@ -9,6 +9,17 @@ const mysql = require("mysql2");
 	multipleStatements: true,
 });*/
 
+/*const db = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	port: process.env.DB_PORT,
+	multipleStatements: true,
+});*/
+
+const fs = require("fs");
+
 const db = mysql.createConnection({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
@@ -16,6 +27,9 @@ const db = mysql.createConnection({
 	database: process.env.DB_NAME,
 	port: process.env.DB_PORT,
 	multipleStatements: true,
+	ssl: {
+		ca: fs.readFileSync(__dirname + "/ca.pem"),
+	},
 });
 
 module.exports = db;
